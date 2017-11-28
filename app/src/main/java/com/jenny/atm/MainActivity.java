143239e19c660658650b1929e795ffbed2e52922
Jenny <a0989783233@gmail.com>
2017-11-28 10:59:51 +0800

    LoginActivity

--- app/src/main/java/com/jenny/atm/MainActivity.java
+++ app/src/main/java/com/jenny/atm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jenny.atm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!logon){
+            Intent intent = new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
