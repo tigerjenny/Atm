8a92a8f150a6729e28be18578516aec174cb4c76
Jenny <a0989783233@gmail.com>
2018-01-02 00:54:16 +0800

    homework city area

--- app/src/main/java/com/jenny/atm/AreaActivity.java
+++ app/src/main/java/com/jenny/atm/AreaActivity.java
@@ -1,11 +1,14 @@
 package com.jenny.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class AreaActivity extends AppCompatActivity {
+public class AreaActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private String[] areas;
     private ListView area;
 
@@ -13,11 +16,33 @@ public class AreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area);
+
         area = (ListView) findViewById(R.id.area);
         int aa = getIntent().getIntExtra("CITY_CODE",0);
-        areas = new String[]{"中正區","暖暖區","八堵區"};
+
+        switch (aa){
+            case 0:
+                areas = new String[]{"中正區","暖暖區","八堵區"};
+                break;
+            case 1:
+                areas = new String[]{"永和區","板橋區","新莊區"};
+                break;
+            case 2:
+                areas = new String[]{"信義區","大安區","士林區"};
+                break;
+        }
+
         ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,areas);
         area.setAdapter(adapter);
+        area.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        getIntent().putExtra("AREA_CODE",areas[i]);
+        setResult(RESULT_OK,getIntent());
+        finish();
 
     }
 }
--- app/src/main/java/com/jenny/atm/CityActivity.java
+++ app/src/main/java/com/jenny/atm/CityActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CityActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
@@ -22,30 +23,28 @@ public class CityActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_city);
 
         ListView city = (ListView) findViewById(R.id.city);
-        cities = new String[] {"基隆市", "新北市","台北市"};
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,cities);
+        cities = new String[]{"基隆市", "新北市", "台北市"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, cities);
         city.setAdapter(adapter);
         city.setOnItemClickListener(this);
+
+    }
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+        //Log.d(TAG, "onItemClick:" + "/" + cities[position]);
+
+        Intent intent = new Intent(this,AreaActivity.class);
+        intent.putExtra("CITY_CODE",position);
+        startActivityForResult(intent,0);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-        //Log.d(TAG, "onItemClick:" + "/" + cities[position]);
-        switch (position) {
-            case 0:
-                Intent intent = new Intent(this, AreaActivity.class);
-                intent.putExtra("CITY_CODE",0);
-                startActivityForResult(intent, 200);
-                break;
-            case 1:
-                Intent intent1 = new Intent(this, Area1Activity.class);
-                startActivity(intent1);
-                break;
-            case 2:
-                Intent intent2 = new Intent(this, Area2Activity.class);
-                startActivity(intent2);
-                break;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            String area = data.getStringExtra("AREA_CODE");
+            Toast.makeText(this,area,Toast.LENGTH_LONG).show();
         }
-
     }
 }
